a1cd5d472d9e3e233193d9631ba7a5c691dfe5e4
Artsemi Stankevich <Artsemi.Stankevich@LeverX.local>
2021-03-10 18:21:20 +0300

    change java version on 11

--- src/main/java/by/dev/mastnail/manage/user/service/converter/UserParamConverter.java
+++ src/main/java/by/dev/mastnail/manage/user/service/converter/UserParamConverter.java
@@ -3,17 +3,16 @@ package by.dev.mastnail.manage.user.service.converter;
 import by.dev.mastnail.manage.user.model.User;
 import by.dev.mastnail.manage.user.param.UserParam;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+import static by.dev.mastnail.util.MapperConfig.getMapper;
 
 @Component
 @RequiredArgsConstructor
 public class UserParamConverter {
 
-    private final ModelMapper modelMapper;
 
     public User toModel(UserParam userParam) {
-        return modelMapper.map(userParam, User.class);
+        return getMapper().map(userParam, User.class);
     }
 
 }
--- src/main/java/by/dev/mastnail/manage/user/web/controller/converter/UserWebConverter.java
+++ src/main/java/by/dev/mastnail/manage/user/web/controller/converter/UserWebConverter.java
@@ -3,17 +3,17 @@ package by.dev.mastnail.manage.user.web.controller.converter;
 import by.dev.mastnail.manage.user.model.User;
 import by.dev.mastnail.manage.user.web.controller.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
+
 import org.springframework.stereotype.Component;
+
+import static by.dev.mastnail.util.MapperConfig.getMapper;
 
 @RequiredArgsConstructor
 @Component
 public class UserWebConverter {
 
-    private final ModelMapper modelMapper;
-
     public UserResponse toUserResponse(User user) {
-        return modelMapper.map(user, UserResponse.class);
+        return getMapper().map(user, UserResponse.class);
     }
 
 }
